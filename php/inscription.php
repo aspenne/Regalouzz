d4d16c42a50429901f05c5759118a0de7bea7010
Nathan <nathanlefol@hotmail.fr>
2022-10-28 11:50:59 +0200

    Mise en pages inscription

--- php/inscription.php
+++ php/inscription.php
@@ -6,31 +6,88 @@
         <script src="../Bootstrap/js/bootstrap.min.js"></script> 
 
     </head>
-    <header>
-        <h1>Alizon</h1>
-    </header>
     <body>  
-        <div class="inscription_box text-center">
+        <div class="inscription_box text-center container" style="width:80%;height:90%">
+            <div class="row justify-content-center">
+                <img src="../img/site/logo.png" style="width:50%;height:auto;">
+            <div class="row justify-content-center mt-2">
                 <h3>Inscrivez-vous:</h3>
+            </div>
                     <form action="" method="post">
-                        <label>Votre nom :</label>
-                        <input type="text" name="nom" required></br>
-                        <label>Prénom </label>
-                        <input type="text" name="prenom" required><br>
-                        <label>Email </label>
-                        <input type="email" name="email" required></br>
-                        <label>tel </label>
-                        <input type="tel"  name="telephone" pattern="06[0-9]{2}[0-9]{2}[0-9]{2}[0-9]{2}" required><br>
-                        <label>Date de naissance </label>
-                        <input type="date" name="date_naissance" required><br>
-                        <label>Mot de passe </label>
-                        <input type="password" name="mdp" required><br>
-                        <label>Confirmer mot de passe </label>  
-                        <input type="password" name="mdp1" required><br>
-                        <input class="button validate" type="submit" value="Valider"><br>
-                        <p class="multiline">Alizon traite les données receuillies à des fins de gestion de la relation client, gestion des commande et des livraisons, pour en savoir plus, veuilez consulter nos <span class="CG">CGU/CGV </span></p>
-                        <h3> Vous avez déjà un compte ?</h3>
-                        <input class="button log" type="submit" value="Identifiez-vous">
+                        <div class="row justify-content-center mt-2">
+                            <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 text-end">
+                                <label>Votre nom :</label>
+                            </div>
+                            <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 text-start">
+                                <input type="text" name="nom" required></br>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center mt-3">
+                            <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 text-end">
+                                <label>Prénom :</label>
+                            </div>
+                            <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 text-start">
+                                <input type="text" name="prenom" required><br>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center mt-3">
+                            <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 text-end">
+                                <label>Email :</label>
+                            </div>
+                            <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 text-start">
+                                <input type="email" name="email" required></br>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center mt-3">
+                            <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 text-end">
+                                <label>Téléphone :</label>
+                            </div>
+                            <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 text-start">
+                                <input type="tel"  name="telephone" pattern="06[0-9]{2}[0-9]{2}[0-9]{2}[0-9]{2}" required><br>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center mt-3">
+                            <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 text-end">
+                                <label>Date de naissance :</label>
+                            </div>
+                            <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 text-start">
+                                <input type="date" name="date_naissance" required><br>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center mt-3">
+                            <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 text-end">
+                                <label>Mot de passe :</label>
+                            </div>
+                            <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 text-start">
+                                <input type="password" name="mdp" required><br>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center mt-3">
+                            <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 text-end">
+                                <label>Confirmer mot de passe :</label>
+                            </div>
+                            <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 text-start">
+                                <input type="password" name="mdp1" required><br>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center mt-3">
+                            <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+                                <input class="button validate" type="submit" value="Valider"><br>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center mt-3">
+                            <div class="col-10 col-sm-10 col-md-6 col-lg-6 col-xl-6">
+                            <p class="multiline">Alizon traite les données receuillies à des fins de gestion de la relation client, gestion des commande et des livraisons, pour en savoir plus, veuilez consulter nos <span class="CG">CGU/CGV </span></p>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center mt-3">
+                            <h3> Vous avez déjà un compte ?</h3>
+                        </div>
+                        <div class="row justify-content-center mt-3">
+                            <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+                                <input class="button log" type="submit" value="Identifiez-vous">
+                            </div>
+                        </div>
                     </form>
         </div>
     </body>
