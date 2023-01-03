47c3fb9963b85ccf2851c254a1dedbf6d3084a4b
aspenne <axel.mch@outlook.fr>
2023-01-03 08:26:39 +0100

    changement identifiant

--- php/id.php
+++ php/id.php
@@ -8,21 +8,21 @@ $user   = 'postgres';
 $pass	= 'password';
 */
 
-
+/*
 $server = 'servbdd.iutlan.etu.univ-rennes1.fr';
 $driver = 'pgsql';
 $dbname = 'pg_amichelo';
 $port = 5432;
 $user = 'amichelo';
 $pass = 'Luffydog-56';
+*/
 
 
-/*é
 $server = '178.32.129.129';
 $driver = 'pgsql';
 $dbname = 'sae';
 $user   = 'axel';
 $pass	= 'Regalouzz';
-*/
+
 
 ?>
