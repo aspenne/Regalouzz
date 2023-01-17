c2d9cc5183e28a413b0d8dd16986763110c6aa90
TorkhanLucas <116345362+TorkhanLucas@users.noreply.github.com>
2023-01-17 14:01:40 +0100

    Update profil.php

--- php/profil.php
+++ php/profil.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <title>Alizon</title>
+    <title>Page Profil</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="../Bootstrap/css/bootstrap.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="../bootstrap/js/jquery-3.5.1.min.js"></script>
-    <link rel="stylesheet" href="../css/style_profil.css">
-    <link rel="stylesheet" href="../css/foot_head.css">
     <script src="../bootstrap/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Profil</title>
+    
+    <link rel="stylesheet" href="../css/style_profil.css">
+    <link rel="stylesheet" href="../css/foot_head.css">
 
 </head>
 <?php include("head.php");?>
