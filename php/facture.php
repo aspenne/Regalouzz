4fc666d22aac24aaf7a502ce7c257753da62c9cf
TorkhanLucas <116345362+TorkhanLucas@users.noreply.github.com>
2023-01-17 09:56:44 +0100

    Add Facture par en pj

--- php/facture.php
+++ php/facture.php
@@ -8,6 +8,7 @@ include('id.php');
 try {
     $dbh = new PDO("$driver:host=$server;dbname=$dbname", $user, $pass,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $id_commande = $_GET['id'];
     $data_adresse_facturation = $dbh->query("SELECT * from Alizon._adresse natural join alizon._commande  WHERE adressefact = id_adresse and id_client='".$_SESSION["id_client"]."'", PDO::FETCH_ASSOC)->fetch();
     $data_client = $dbh->query("SELECT * FROM alizon._client  WHERE id_client='".$_SESSION["id_client"]."'", PDO::FETCH_ASSOC)->fetch();
 
@@ -21,6 +22,37 @@ catch (PDOException $e) {
     print "Erreur !: " . $e->getMessage() . "";
     die();
 }
+
+?>
+<html>
+    <head>
+        <title>Connexion</title>
+        <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../css/inscription_connexion.css">
+        <script src="../bootstrap/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    </head>
+<header>
+    <div class="container">
+        <div class="row justify-content-center">
+            <img src="../img/site/logo.png" style="width:50%;height:auto;cursor:pointer;" onclick="window.location.href='./Liste_produit.php'">
+        </div>
+    </div>
+</header>
+<body>
+    <div class="inscription_box text-center container" style="width:80%;height:90%">
+        <!-- <div class="row justify-content-center">
+            <img src="../img/site/logo.png" style="width:50%;height:auto;cursor:pointer;" onclick="window.location.href='./Liste_produit.php'">
+        </div> -->
+        <div class="row justify-content-center mt-2">
+                <h3>Confirmation Commande :</h3>
+            </div>
+                        <div class="row justify-content-center mt-3">
+                            <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 text-center">
+
+
+<?php
 
 class PDF extends FPDF {
     // Page header
@@ -165,8 +197,144 @@ $header = array('Quantite', 'Libelle', 'Prix Unit. HT  '.EURO, 'Montant HT  '.EU
 //$pdf->BasicTable($header, $res);
 $pdf->FancyTable($header, $res);
 
-$pdf->Output();
+$pdf->Output("mon_pdf.pdf", "F");
 
 
+// Confirmation par mail
+    // Contenu du mail : 
+    // Récupérez le contenu du PDF
 
+     //-----------------------------------------------
+     //DECLARE LES VARIABLES
+     //-----------------------------------------------
+ 
+     $from = 'cobrec.alizon@gmail.com';
+     $nom = 'Confirmation Commande - Alizon';
+     $sujet ='Votre Facture Alizon';
+     $sujet = html_entity_decode($sujet);
+     #$sujet = $sujet;
+    
+     #include($root.'include/fpdf16/mod6.php');
+  
+  
+      //-----------------------------------------------
+      //GENERE LA FRONTIERE DU MAIL ENTRE TEXTE ET HTML
+      //-----------------------------------------------
+  
+     $eol = PHP_EOL;
+     $limite = md5(uniqid(microtime(), TRUE));
+     #$limite = "----------=_parties_".md5(uniqid (rand()));
+  
+  
+      //-----------------------------------------------
+      //HEADERS DU MAIL
+      //-----------------------------------------------
+  
+     $header  = 'Reply-to: '.$from.$eol;
+     $header .= 'From: '.$from.$eol;
+
+     $header .= 'Return-Path: '.$from.$eol;
+     #$header .= 'X-Confirm-Reading-To: '.$from.$eol;
+    $header .= 'X-Sender: '.$eol;
+     $header .= 'X-Mailer: PHP/5.3.2'.$eol;
+     $header .= 'X-auth-smtp-user: '.$from.$eol;
+     $header .= 'X-abuse-contact: '.$from.$eol;
+     $header .= 'X-Spam-Status: No'.$eol;
+     $header .= 'Date: '.date('D, j M Y G:i:s O').$eol;
+     $header .= 'MIME-Version: 1.0'.$eol;
+     $header .= 'Content-Type: multipart/mixed; boundary='.$limite.$eol;
+     #$header .= 'Content-Type: multipart/alternative; boundary="'.$limite.'"'."\r\n";
+  
+  
+      //-----------------------------------------------
+      //MESSAGE TEXTE
+      //-----------------------------------------------
+  
+     #$message_ = "";
+  
+     $message_ = '--'.$limite.$eol;
+     
+     
+       $message_ .= 'Texte affiché par des clients mail ne supportant pas le type MIME.'.$eol.$eol;
+     
+  
+      //-----------------------------------------------
+      //MESSAGE HTML
+      //-----------------------------------------------
+  
+     $message_ .= '--'.$limite.$eol;
+     $message_ .= 'Content-type: text/html; charset=utf-8'.$eol.$eol;
+
+  
+  
+  
+    //------
+    // PIECE JOINTE
+    //-------------
+        $filename = 'mon_pdf.pdf';
+     if (file_exists($filename)){
+         #$file_type = filetype($filename);
+         #$file_size = filesize($filename);
+  
+         $message_ .= '--'.$limite.$eol;
+         $message_ .= 'Content-Type: application/pdf; name="message.pdf"'.$eol;
+         #$message_ .= 'Content-Type: application/octet-stream; name="uuu'.$filename.'"'.$eol;
+         $message_ .= 'Content-Transfer-Encoding: base64'.$eol;
+         $message_ .= 'Content-Disposition: attachment'.$eol.$eol; 
+         $message_ .= chunk_split(base64_encode(file_get_contents($filename))).$eol.$eol;
+  
+  
+     }else{echo 'existe pas';}
+  
+  
+  
+     $message_ .= '--'.$limite.'--'.$eol;
+  
+      //-----------------------------------------------
+      //ENVOI
+      //-----------------------------------------------
+    $recup = $dbh->prepare("SELECT mail FROM alizon._client WHERE id_client = ?");
+    $recup->execute([$_SESSION['id_client']]);
+    $resRecup = $recup->fetch(PDO::FETCH_ASSOC);
+    foreach($resRecup as $row){
+    if(mail($row, $sujet, $message_, $header)){
+        echo 'Un mail de confirmation de votre commande avec votre facture en pièce jointe a été envoyé.';
+    } 
+    else {
+        echo 'Une erreur est survenue';
+    }
+}
+  
+  
 ?>
+                            </div>
+                        </div>
+                        <?php
+                            if(isset($_POST['error'])){
+                                echo '<div class="row justify-content-center mt-3">
+                                        <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 text-end">
+                                            <label style="color : red">Erreur :</label>
+                                        </div>
+                                        <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 text-start">
+                                            <label style="color : red">Identifiants incorrects</label>
+                                        </div>
+                                    </div>';
+                            }
+                        ?>
+                        <div class="row justify-content-center mt-3">
+                            <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+                                <?php
+                                    echo "<button type='button' onclick='location.href=\"facture_ligne.php?id=".$id_commande."\"'>Consulter Facture</button>";
+                                ?>
+                            </div>
+                            <div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+                                <?php
+                                    echo "<button type='button' onclick='location.href=\"recap.php?id=".$id_commande."\"'>Retour sur Alizon</button>";
+                                ?>
+                            </div>
+                        </div>
+        </div>
+    </body>
+    <footer>
+
+    </footer>
