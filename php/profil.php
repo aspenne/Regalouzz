d40af094f47b40b78e1d8646b61d5d4329983d31
TorkhanLucas <116345362+TorkhanLucas@users.noreply.github.com>
2023-01-17 11:02:25 +0100

    Update profil.php

--- php/profil.php
+++ php/profil.php
@@ -4,7 +4,7 @@
     <title>Alizon</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="../Bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="../bootstrap/js/jquery-3.5.1.min.js"></script>
     <link rel="stylesheet" href="../css/style_profil.css">
