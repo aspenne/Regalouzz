93d025b790b042208aa171f9713b6faeeb14200f
TorkhanLucas <116345362+TorkhanLucas@users.noreply.github.com>
2023-01-17 11:03:17 +0100

    Update recherche.php

--- php/recherche.php
+++ php/recherche.php
@@ -4,9 +4,6 @@
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
     <script src="../bootstrap/js/bootstrap.min.js"></script>
 
-    <link rel="stylesheet" href="../css/style_prod.css">
-    <link rel="stylesheet" href="../css/foot_head.css">
-
     <link rel="stylesheet" href="../bootstrap/js/bootstrap.js"/>
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.css"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
@@ -16,6 +13,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
+    <link rel="stylesheet" href="../css/style_prod.css">
+    <link rel="stylesheet" href="../css/foot_head.css">
   </head>
 <?php
 include('id.php');
