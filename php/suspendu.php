f079803bc8e10ad512f29b9624814bbfacdafe28
aspenne <axel.mch@outlook.fr>
2023-01-20 16:36:28 +0100

    display for a suspend user

---
+++ php/suspendu.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Connexion</title>
+        <link rel="stylesheet" href="../Bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../css/inscription_connexion.css">
+        <script src="../Bootstrap/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    </head>
+<header>
+    <div class="container">
+        <div class="row justify-content-center">
+            <img src="../img/site/logo.png" style="width:50%;height:auto;cursor:pointer;" onclick="window.location.href='./Liste_produit.php'">
+        </div>
+    </div>
+</header>
+<body>
+<div class="row justify-content-center mt-2" style="text-align : center;">
+    <h1> Votre Compte à été suspendu </h1>
+    <h2> Veuillez contacter le service client à l'adresse suivante : <label>Contact@cobrec.fr</label> pour plus d'informations </h2>
+</div>
+</body>
+</html>
