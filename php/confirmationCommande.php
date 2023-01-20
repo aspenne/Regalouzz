d84d581066e6674275a34f2065c5b658799acef1
aspenne <axel.mch@outlook.fr>
2023-01-20 17:11:41 +0100

    fixing bugs

--- php/confirmationCommande.php
+++ php/confirmationCommande.php
@@ -5,10 +5,12 @@
         $dbh = new PDO("$driver:host=$server;dbname=$dbname", $user, $pass);
         $stmt = $dbh->prepare("INSERT INTO Alizon._ReassortVendeur(id_produit,id_vendeur,date_commande,quantite,etat) VALUES (?,?,?,?,'en_cours')");
         $stmt->execute([$_GET["id_produit"],$_GET['id_vendeur'],date("Y-m-d"),$_GET['qte_commande']]);
-        echo '<script type="text/javascript">
+        ?>
+        <script type="text/javascript">
             alert("Votre réassort a été pris en compte");
-            </script>';
-        header('location:detail_produit.php?ID='.$_GET['id_produit']);
+            window.location.href = "detail_produit.php?ID=" + <?php echo $_GET['id_produit'] ?>;
+        </script>';
+        <?php
     } catch (PDOException $e) {
         print "Erreur !: " . $e->getMessage() . "<br/>";
         die();
