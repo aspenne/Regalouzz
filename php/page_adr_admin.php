aa9a7a66f2e5801d56a2191f9db2baa7564fb9f1
aspenne <axel.mch@outlook.fr>
2023-01-20 17:19:02 +0100

    add display for all client account from the seller's part

---
+++ php/page_adr_admin.php
@@ -0,0 +1,62 @@
+<html>
+    <head>
+        <title>Adresse</title>
+        <link rel="stylesheet" href="../Bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../css/style.css">
+        <script src="../Bootstrap/js/bootstrap.min.js"></script> 
+        <link rel="stylesheet" href="../css/style_profil.css">
+        <link rel="stylesheet" href="../css/foot_head.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    </head>
+    <?php
+    include("head.php");
+    if(!isset($_SESSION['admin']) or $_SESSION['admin'] != TRUE){
+        header("location:./Liste_produit.php");
+    }
+    include('id.php');
+    ?>
+<body>
+    <main>
+    <?php
+try {
+    $dbh = new PDO("$driver:host=$server;dbname=$dbname", $user, $pass,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+    $stmt = $dbh->prepare("SELECT * FROM alizon._adresse WHERE id_client = ".$_GET["id_client"]." ORDER BY id_adresse");
+    $stmt->execute();
+    $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo '<h2 id="titre"> Vos Adresses </h2>';
+    echo '<button type="submit" onclick="window.location.href = \'page_adr_add_admin.php?id_client='.$_GET['id_client'].'\'" class="button" id="btn">Ajouter une adresse</button>';
+    echo '<div class="container">';
+    echo '<div class="row justify-content-center">';
+    $cpt = 0;
+    
+    foreach ($res as $adr){
+        $cpt +=1;
+        echo "<div class='adresse'>
+            <p> Adresse n° ".$cpt." </p>
+            <p> Rue : ".$adr['num']. " ". $adr['rue']."</p>
+            <p> Code Postal : ".$adr['code_postal']."</p>
+            <p> Ville : ".$adr['ville']."</p>
+            <p> Pays : ".$adr['pays']."</p>
+            <button type='submit' onclick='window.location.href = \"page_adresse_admin.php?id=".$adr['id_adresse']."&id_client=".$_GET['id_client']."\"' class='button' id='mod'>Modifier</button>
+        </div>";
+        
+    }
+    echo '</div>
+    </div>
+    <button type="submit" onclick="window.location.href = \'modifier_client.php?id_client='.$_GET['id_client'].'\'" class="button" id="btn2">Retour profil</button>';
+
+}
+catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+?> 
+</main>
+</body>
+<?php
+include('footer.php');
+?>
+</html>
